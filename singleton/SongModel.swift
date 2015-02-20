f2b28f49f71b05c6803d6226e890851827a40a59
John Barr <barr@ithaca.edu>
2015-02-19 22:41:44 -0500

    First commit

---
+++ singleton/SongModel.swift
@@ -0,0 +1,31 @@
+//
+//  SongModel.swift
+//  Songs
+//
+//  Created by Evan Sobkowicz on 2/1/15.
+//  Copyright (c) 2015 Evan Sobkowicz. All rights reserved.
+//
+
+import Foundation
+
+
+class Song {
+    
+    var name: String
+    let artist: String
+    let album: String
+    let year: Int
+    let composer: String
+    let length: Float
+    
+    /* Default initializer
+    */
+    init(){
+        self.name = "none"
+        self.artist = "none"
+        self.album = "none"
+        self.year = 2000
+        self.composer = "none"
+        self.length = 0.0
+    }
+}
